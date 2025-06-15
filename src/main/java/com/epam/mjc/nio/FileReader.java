0551ee88258df9f7eda180658bef64f4dfc5364b
KonstantineVasahalomize <vashalomidzekonstantine@gmail.com>
2025-06-15 14:52:10 +0400

    tests pass

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = null, email = null;
+        int age = 0;
+        long phone = 0;
+        try (BufferedReader bufferedReader = Files.newBufferedReader(file.toPath())) {
+            String line = bufferedReader.readLine();
+            final String nameLabel = "Name: ";
+            final String ageLabel = "Age: ";
+            final String emailLabel = "Email: ";
+            final String phoneLabel = "Phone: ";
+            while (line != null) {
+                if (line.startsWith(nameLabel)) {
+                    name = line.substring(nameLabel.length());
+                } else if (line.startsWith(ageLabel)) {
+                    age = Integer.parseInt(line.substring(ageLabel.length()));
+                } else if (line.startsWith(emailLabel)) {
+                    email = line.substring(emailLabel.length());
+                } else {
+                    phone = Long.parseLong(line.substring(phoneLabel.length()));
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("not a number");
+            return null;
+        } catch (IOException e) {
+            System.out.println("error");
+            return null;
+        }
+        return new Profile(name, age, email, phone);
     }
 }
